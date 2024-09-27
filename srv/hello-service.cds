46b48e0f58416e325ffc4e43b98d3e7db6fbd7ae
CARLOS Jose Avelino <carlos.avelino.prestserv@petrobras.com.br>
2024-09-27 18:31:45 +0000

    Added function sayHelloA

--- srv/hello-service.cds
+++ srv/hello-service.cds
@@ -1,3 +1,4 @@
-service HelloService {
-    function sayHello(name : String) returns String;
+service HelloService2 {
+    function sayHello(name : String, name2 : String) returns String;
+    function sayHelloA(name : String, name2 : String) returns String;
 }
--- srv/hello-service.js
+++ srv/hello-service.js
@@ -1,7 +1,37 @@
 const cds = require('@sap/cds');
 
-class HelloService extends cds.ApplicationService {
-    sayHello(name) { return "Hello " + name; }
+/* class HelloService extends cds.ApplicationService {
+    sayHello(name) { return "@Hello " + name; }
+} */
+ class HelloService extends cds.ApplicationService {
+    sayHello(name, name2) { 
+        console.log(this.sayHello2(name, name2));
+        return this.sayHello2(name, name2);         
+    }
+
+    sayHello2(name, name2) {
+        return "Hello " + name + ' ' + name2;
+    }
+
 }
 
+
+class HelloService2 extends cds.ApplicationService {
+    sayHelloA(name, name2) { 
+        console.log(this.sayHelloB(name, name2));
+        return this.sayHelloB(name, name2);         
+    }
+
+    sayHelloB(name, name2) {
+        return "Hello " + name + ' ' + name2;
+    }
+
+}
+
+
+/* class HelloService extends cds.ApplicationService {
+    sayHello2(name) { this.sayHello2; }
+} */
+
 module.exports = HelloService;
+module.exports = HelloService2;
